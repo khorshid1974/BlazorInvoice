00fce9308913571e25a4f0207f304182fc92f3c0
khorshid1974 <khorshid.hamza@epu.edu.iq>
2024-06-18 22:30:31 +0300

    Adding Styles

--- BlazorInvoice/Program.cs
+++ BlazorInvoice/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddSqlServer<InvoiceDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 builder.Services.AddQuickGridEntityFrameworkAdapter();
 var app = builder.Build();
